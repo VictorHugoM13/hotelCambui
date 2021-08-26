3a22bca87994bcc4bf9975370f7a64b0e199280a
MICRO <victor_osk8@hotmail.com>
2021-08-25 22:16:11 -0300

    limpando e validando url

--- core/ConfigController.php
+++ core/ConfigController.php
@@ -1,35 +1,68 @@
 <?php
 namespace Core;
-class ConfigController {
+
+class ConfigController
+{
     private $Url;
     private $UrlConjunto;
     private $UrlController;
     private $UrlParametro;
+    private static $Format;
 
-    public function __construct(){
+    public function __construct() {
+        if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))) {
+            $this->Url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
 
-        $this->Url =  filter_input(INPUT_GET,'url', FILTER_DEFAULT);
-        $this->UrlConjunto = explode("/", $this->Url);
-        if (isset($this->UrlConjunto[0])) {
-            $this->UrlController = $this->UrlConjunto[0];
+            $this->limparUrl();
+
+            $this->UrlConjunto = explode("/",$this->Url);
+
+            if(isset($this->UrlConjunto[0])){
+                $this->UrlController = $this->slug($this->UrlConjunto[0]);
+            }
+            else {
+                $this->UrlController = "Home";
+            }
+
+            if(isset($this->UrlConjunto[1])) {
+                $this->UrlParametro = $this->slug($this->UrlConjunto[1]);
+            }
+            else {
+                $this->UrlParametro = null;
+            }
         }
         else {
-            $this->UrlController = 'Home';
-        }
-
-        if (isset($this->UrlConjunto[1])) {
-            $this->UrlParametro = $this->UrlConjunto[1];
-        }
-        else {
+            $this->UrlController = "Home";
             $this->UrlParametro = null;
         }
 
+        echo "URL: " . $this->Url ."<br>";
+        echo "Url Controller: " . $this->UrlController ."<br>";
+        echo "Url Parametro: " . $this->UrlParametro ."<br>";
 
-        echo "<br>URL: " . $this->Url;
-        echo "<br>URL: " . $this->UrlConjunto;
+    }
+    private function limparUrl() {
+        //eliminar tags
+        $this->Url = strip_tags($this->Url);
+        //eliminar espacos
+        $this->Url = trim($this->Url);
+        //eliminar ultima barra
+        $this->Url = rtrim($this->Url, "/");
+        self::$Format = array();
+        self::$Format['acentos'] = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+~={[}]?;:.,\\\'<>°ºª ';
+        self::$Format['sem_acentos'] = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyrr----------------------------------';
+        $this->Url = strtr(utf8_decode($this->Url), utf8_decode(self::$Format['acentos']), self::$Format['sem_acentos']);
+        $this->Url = strtolower($this->Url);
+    }
 
-
-        echo "<br>URL Parametro: " . $this->UrlParametro;
-
+    private function slug($UrlController) {
+        $UrlController = strtolower($UrlController);
+        //Substituir - por " "
+        $UrlController = str_replace("-", " ", $UrlController);
+        //Primeira letra em maiuscula
+        $UrlController = ucwords($UrlController);
+        //Substituir " " por ""
+        $UrlController = str_replace(" ", "", $UrlController);
+        return $UrlController;
     }
 }
