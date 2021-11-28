1316c478e473621857d3ec07717acb0c739d824f
Victor Hugo <victor_osk8@hotmail.com>
2021-11-28 10:41:07 -0300

    bd

--- hotelcambui.sql
+++ hotelcambui.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Tempo de geração: 09-Nov-2021 às 22:51
+-- Tempo de geração: 28-Nov-2021 às 13:37
 -- Versão do servidor: 10.4.13-MariaDB
 -- versão do PHP: 7.3.21
 
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `sts_carousels` (
 --
 
 INSERT INTO `sts_carousels` (`id`, `nome`, `imagem`, `titulo`, `descricao`, `posicao_text`, `titulo_botao`, `link`, `ordem`, `adms_cor_id`, `adms_sit_id`, `created`, `modified`) VALUES
-(1, 'GRADUAÇÃO PRESENCIAL', 'imagem_um.jpg', 'Hotel Cambuí', 'Realize um experiência incrível!', 'text-left', 'Relize uma reserva!', 'http://localhost/siteHotel/Reservas', 1, 5, 1, '2018-05-23 00:00:00', '2020-08-19 12:23:35'),
+(1, 'GRADUAÇÃO PRESENCIAL', 'imagem_um.jpg', 'Hotel Extrema', 'Realize um experiência incrível!', 'text-left', 'Relize uma reserva!', 'http://localhost/siteHotel/Reservas', 1, 5, 1, '2018-05-23 00:00:00', '2020-08-19 12:23:35'),
 (2, 'Segundo Exemplo', 'imagem_dois.jpg', 'Hotel Cambuí', 'O mais completo da cidade! ', 'text-left', 'Relize uma reserva!', 'http://localhost/siteHotel/Reservas', 2, 5, 1, '2018-05-23 00:00:00', '2020-08-20 23:21:04'),
 (3, 'Terceiro Exemplo', 'imagem_tres.jpg', 'Hotel Cambuí', 'Descanse no melhor lugar da cidade!', 'text-left', 'Relize uma reserva!', 'http://localhost/siteHotel/Reservas', 3, 5, 1, '2018-05-23 00:00:00', '2020-08-19 23:31:34');
 
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS `sts_historia` (
 --
 
 INSERT INTO `sts_historia` (`id`, `titulo`, `imagem`, `descricao`) VALUES
-(1, 'Nosso Hotel', 'nosso_hotel.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non sem nec metus vehicula scelerisque. In quis sapien quis eros bibendum laoreet vitae et ipsum. Sed ex nisl, rhoncus quis finibus ac, sagittis in purus. Etiam et volutpat ligula. Nullam at erat mauris. Morbi felis neque, mattis ac vulputate quis, ultrices a augue. Donec at risus ut turpis egestas posuere non non ipsum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non sem nec metus vehicula scelerisque. In quis sapien quis eros bibendum laoreet vitae et ipsum. Sed ex nisl, rhoncus quis finibus ac, sagittis in purus. Etiam et volutpat ligula. Nullam at erat mauris. Morbi felis neque, mattis ac vulputate quis, ultrices a augue. Donec at risus ut turpis egestas posuere non non ipsum. ');
+(1, 'Nosso Hotel', 'nosso_hotel.jpg', '10 anos de boas vindas calorosas...\r\nSua opção de um lugar simples mas com conforto e aconchego nas montanhas\r\n\r\nTradicional hotel de Cambuí, situado a 200 metros do centro, nosso hotel vem sendo sempre renovando para melhor atender seus hospedes, elevando a qualidade da sua experiência conosco. Nossa proposta é proporcionar uma acolhida confortável e intimista. Nosso hotel dispõe de recepção 24h, sala de leitura, lounge-bar, sala de TV com lareira, salão de jogos com mesas de bilhar e tênis de mesa, piscina coberta e solarium.');
 
 -- --------------------------------------------------------
 
@@ -135,6 +135,77 @@ CREATE TABLE IF NOT EXISTS `sts_sobre` (
 
 INSERT INTO `sts_sobre` (`id`, `titulo_principal`, `titulo_secundario_um`, `descricao_um`, `imagem_um`, `titulo_secundario_dois`, `descricao_dois`, `imagem_dois`, `titulo_secundario_tres`, `descricao_tres`, `imagem_tres`) VALUES
 (1, 'Sobre o Hotel', 'Quartos', 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', 'sobre_um.jpg', 'Restaurante', 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', 'sobre_dois.jpg', 'Lazer', 'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', 'sobre_tres');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_cliente`
+--
+
+DROP TABLE IF EXISTS `tb_cliente`;
+CREATE TABLE IF NOT EXISTS `tb_cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpf` char(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_cliente`
+--
+
+INSERT INTO `tb_cliente` (`id`, `cpf`, `nome`, `email`, `senha`) VALUES
+(1, '11879934680', 'Victor', 'victor_osk8@hotmail.com', '123'),
+(2, '11879934680', 'joao', 'joao@gmail.com', '123'),
+(3, '11879934680', 'Victor', 'victor_osk8@hotmail.com', '123'),
+(4, '11879934680', 'Victor', 'victor_osk8@hotmail.com', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_quarto`
+--
+
+DROP TABLE IF EXISTS `tb_quarto`;
+CREATE TABLE IF NOT EXISTS `tb_quarto` (
+  `numero_quarto` char(2) NOT NULL,
+  `tipo_quarto` varchar(30) DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `valor_diaria` float DEFAULT NULL,
+  `capacidade_quarto` int(11) DEFAULT NULL,
+  `status_quarto` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`numero_quarto`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_quarto`
+--
+
+INSERT INTO `tb_quarto` (`numero_quarto`, `tipo_quarto`, `descricao`, `valor_diaria`, `capacidade_quarto`, `status_quarto`) VALUES
+('1', 'Suite Solteiro', 'Um quarto simples mas muito aconchegante com uma cama solteiro, suite, serviços como TV a Cabo, wifi inclusos', 100, 1, 'disponivel'),
+('2', 'Suite Casal', 'Um quarto simples mas com muito conforto para o casal com uma cama de casal, suite, serviços como TV a Cabo, wifi inclusos', 150, 2, 'disponivel'),
+('3', 'Suite duplo Casal', 'Um quarto simples mas com muito conforto para os casais com duas cama de casal, suite, serviços como TV a Cabo, wifi inclusos', 200, 4, 'disponivel');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_reserva`
+--
+
+DROP TABLE IF EXISTS `tb_reserva`;
+CREATE TABLE IF NOT EXISTS `tb_reserva` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) DEFAULT NULL,
+  `numero_quarto` int(11) DEFAULT NULL,
+  `checkin` date DEFAULT NULL,
+  `checkout` date DEFAULT NULL,
+  `valor_toal` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `numero_quarto` (`numero_quarto`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
