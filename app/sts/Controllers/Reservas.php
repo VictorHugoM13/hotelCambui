f20dd43e98d2049f2b97803a6d6b822e326191e1
Victor Hugo <victor_osk8@hotmail.com>
2022-02-04 21:36:43 -0300

    autentifição de sessao para reservas

--- app/sts/Controllers/Reservas.php
+++ app/sts/Controllers/Reservas.php
@@ -18,15 +18,24 @@
 
         public function index() {
             $this->Dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+            
             if (!empty($this->Dados['CadReservas'])) {
                 unset($this->Dados['CadReservas']);
                 unset($this->Dados['nome']);
                 unset($this->Dados['email']);
-                $reserva = new \Sts\Models\helper\StsCreate();
-                $reserva->execCreate('tb_reserva', $this->Dados);
-                echo "<script>alert('Reserva realizada com Sucesso!');</script>";
-                header('Location: '. URL . 'home');
+
+                if (!isset($_SESSION["id"])) {
+                    header('Location: ' . URL . 'Cliente');
+                }
+                else{
+                    $reserva = new \Sts\Models\helper\StsCreate();
+                    $reserva->execCreate('tb_reserva', $this->Dados);
+                    echo "<script>alert('Reserva realizada com Sucesso!');</script>";
+                    header('Location: '. URL . 'home');
+                }
+                
             }
+            
 
             $modelQuartos= new \Sts\Models\StsQuartos();
             $this->Dados['tb_quarto'] = $modelQuartos->index();
@@ -51,7 +60,6 @@
             }
             $modelQuartos= new \Sts\Models\StsQuartos();
             $this->Dados['tb_quarto'] = $modelQuartos->index();
-
         }
 
 
